d3f613bcad4ea8ef3014f89d8e25f3bde18a1d5e
Yusuf <yusufhameed911@gmail.com>
2024-08-16 16:15:08 +0530

    sgsggsgs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.post("/api", (req, res) => {
+  res.send("Welcome to the Real World!");
+});
 // start the server
 app.listen(3000, () => {
   console.log("Server started on port 3000");
